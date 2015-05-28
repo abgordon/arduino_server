b719c7cc13e48e0336123c1db246f288a2e7851d
Zandrr <alexander.campbell@colorado.edu>
2015-05-28 17:51:08 -0400

    fixed serve static thingy ;)

--- frontend/server/routes.js
+++ frontend/server/routes.js
@@ -6,13 +6,17 @@ module.exports = function(app){
 
 
 
-	app.get('/', function(req,res){
+	// app.get('/', function(req,res){
 
-		Sensor_data.find().exec(function(err, leads){
-			//console.log(leads[0])
-	  res.render('index', { title: 'Bloom', message: 'Current Reading:', json_info: leads});
-		});
-	});
+	// 	Sensor_data.find().exec(function(err, leads){
+	// 		//console.log(leads[0])
+	//   res.render('index', { title: 'Bloom', message: 'Current Reading:', json_info: leads});
+	// 	});
+	// });
+
+  app.get('/', function(req, res) {
+    res.render('index');
+  });
 
 
 
@@ -22,35 +26,35 @@ module.exports = function(app){
 	// ***** default stuff ***** //
 
 	// catch 404 and forward to error handler
-	app.use(function(req, res, next) {
-	  var err = new Error('Not Found');
-	  err.status = 404;
-	  next(err);
-	});
+	// app.use(function(req, res, next) {
+	//   var err = new Error('Not Found');
+	//   err.status = 404;
+	//   next(err);
+	// });
 
 	// error handlers
 
 	// development error handler
 	// will print stacktrace
-	if (app.get('env') === 'development') {
-	  app.use(function(err, req, res, next) {
-	    res.status(err.status || 500);
-	    res.render('error', {
-	      message: err.message,
-	      error: err
-	    });
-	  });
-	}
+	// if (app.get('env') === 'development') {
+	//   app.use(function(err, req, res, next) {
+	//     res.status(err.status || 500);
+	//     res.render('error', {
+	//       message: err.message,
+	//       error: err
+	//     });
+	//   });
+	// }
 
 	// production error handler
 	// no stacktraces leaked to user
-	app.use(function(err, req, res, next) {
-	  res.status(err.status || 500);
-	  res.render('error', {
-	    message: err.message,
-	    error: {}
-	  });
-	});
+	// app.use(function(err, req, res, next) {
+	//   res.status(err.status || 500);
+	//   res.render('error', {
+	//     message: err.message,
+	//     error: {}
+	//   });
+	// });
 }
 
 
@@ -68,5 +72,4 @@ module.exports = function(app){
 // });
 
 // module.exports = router;
-
 
