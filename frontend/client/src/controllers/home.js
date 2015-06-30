2b3740b8f80f000f45a4ae1b9d35c0948bba14ea
abgordon <andrew.b.gordon@colorado.edu>
2015-06-30 13:19:15 -0600

    changed axes, start new dataset

--- frontend/client/src/controllers/home.js
+++ frontend/client/src/controllers/home.js
@@ -6,7 +6,8 @@ angular.module('app').controller('HomeController', [
   function($scope,$http, $interval){
 
     $scope.sent = [0];
-    $scope.check_data = []
+    $scope.check_temp_data = []
+    $scope.check_humidity_data = []
     // $scope.query = function(){
     // 	$http({
     // 		method: 'GET',
@@ -56,13 +57,13 @@ angular.module('app').controller('HomeController', [
 	// }
 
 
-	$scope.drawGraph = function(sent){
+	$scope.drawTempGraph = function(sent){
 		var data = sent.data;
 		var sorted = sent
 		var tsv_data = []
-
+		console.log("initializing graph with " + data.length + " data points.....");
 		if (sent.data.length == 0){console.log("No user found!");return;}
-		else if ($scope.check_data == data){console.log("stopping propogation...");return;}
+		else if ($scope.check_temp_data == data){console.log("stopping propogation...");return;}
 		else {
 			$( "#graph" ).empty();
 			
@@ -70,7 +71,7 @@ angular.module('app').controller('HomeController', [
 
 
 			//create d3 date parser
-			var parseDate = d3.time.format("%d-%b-%y").parse;
+			var parseDate = d3.time.format("%d-%b-%y-%I-%M-%S").parse;
 
 
 			//convert timestamp to appropriate d3 format
@@ -81,11 +82,16 @@ angular.module('app').controller('HomeController', [
 				var month = months[timestamp.getMonth()];
 				var date = timestamp.getDate();
 				var year = timestamp.getFullYear()-2000;
-				var seconds = timestamp.getSeconds()
-				var formatted_time = date + '-'+ month + '-' + year;
+			    var hour = timestamp.getHours();
+			    var min = timestamp.getMinutes();
+			    var sec = timestamp.getSeconds();
+				var formatted_time = date + '-'+ month + '-' + year + '-' + hour + '-' + min + '-' + sec;
 				var temp_c = (d.temp_c);
+				var temp_f = (+temp_c)*1.8 + 32
+				var h = (+(d.rel_h))*0.01;
 				currObj['date']= parseDate(formatted_time);
-				currObj['temp']= (+temp_c)*1.8 + 32
+				currObj['temp']= temp_f;
+				//currObj['rel_h'] = temp_f*h;
 				tsv_data.push(currObj);
 			});
 
@@ -127,7 +133,7 @@ angular.module('app').controller('HomeController', [
 						  x.domain(d3.extent(tsv_data, function(d) { return d.date; }));
 						  y.domain(d3.extent(tsv_data, function(d) { return d.temp; }));
 
-
+						  x.domain += 10; // why the FUCK does this make the font cleaner
 						  svg.append("g")
 						      .attr("class", "x axis")
 						      .attr("transform", "translate(0," + height + ")")
@@ -150,14 +156,121 @@ angular.module('app').controller('HomeController', [
 			//end forEach()
 				});
 		
-			$scope.check_data = data;
+			$scope.check_temp_data = data;
 			}
-
-  		
-
+ 		
 	//end drawGraph(data)
 	}
-   
+
+ 	$scope.drawHumidityGraph = function(sent){
+		//var data = sent.data;
+		var l = sent.data.length
+		var data = []
+		for (i=0;i<l; i=i+100){
+			if (sent.data[i] != undefined){
+			data.push(sent.data[i])
+			}
+		}
+		var sorted = sent
+		var tsv_data = []
+		console.log("initializing graph with " + data.length + " data points.....");
+		if (sent.data.length == 0){console.log("No user found!");return;}
+		else if ($scope.check_humidity_data == data){console.log("stopping propogation...");return;}
+		else {
+			$( "#graph" ).empty();
+			
+
+
+
+			//create d3 date parser
+			var parseDate = d3.time.format("%d-%b-%y-%I-%M-%S").parse;
+
+
+			//convert timestamp to appropriate d3 format
+			data.forEach(function(d){ 				
+				var currObj = {}
+				var timestamp= new Date(d.timestamp * 1000);
+				var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+				var month = months[timestamp.getMonth()];
+				var date = timestamp.getDate();
+				var year = timestamp.getFullYear()-2000;
+			    var hour = timestamp.getHours();
+			    var min = timestamp.getMinutes();
+			    var sec = timestamp.getSeconds();
+				var formatted_time = date + '-'+ month + '-' + year + '-' + hour + '-' + min + '-' + sec;
+				var temp_c = (d.temp_c);
+				var temp_f = (+temp_c)*1.8 + 32
+				var h = (+(d.rel_h))*0.01;
+				currObj['date']= parseDate(formatted_time);
+				currObj['rel_h'] = h;
+				tsv_data.push(currObj);
+			});
+
+				
+			//set bounds
+			var margin = {top: 20, right: 20, bottom: 30, left: 50},
+			    width = 960 - margin.left - margin.right,
+			    height = 500 - margin.top - margin.bottom;
+
+			var x = d3.time.scale()
+			    .range([0, width]);
+
+			var y = d3.scale.linear()
+			    .range([height, 0]);
+
+			var xAxis = d3.svg.axis()
+			    .scale(x)
+			    .orient("bottom");
+
+			var yAxis = d3.svg.axis()
+			    .scale(y)
+			    .orient("left");
+
+			var line = d3.svg.line()
+			    .x(function(d) { return x(d.date); })
+			    .y(function(d) { return y(d.rel_h); });
+
+			var svg = d3.select("#graph").append("svg")
+			    .attr("width", width + margin.left + margin.right)
+			    .attr("height", height + margin.top + margin.bottom)
+			  .append("g")
+			    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+			//draw graph
+			console.log("attempting to write from TSV....")
+				tsv_data.forEach(function(d){
+
+						  x.domain(d3.extent(tsv_data, function(d) { return d.date; }));
+						  y.domain(d3.extent(tsv_data, function(d) { return d.rel_h; }));
+
+
+						  svg.append("g")
+						      .attr("class", "x axis")
+						      .attr("transform", "translate(0," + height + ")")
+						      .call(xAxis);
+
+						  svg.append("g")
+						      .attr("class", "y axis")
+						      .call(yAxis)
+						    .append("text")
+						      .attr("transform", "rotate(-90)")
+						      .attr("y", 6)
+						      .attr("dy", ".71em")
+						      .style("text-anchor", "end")
+						      .text("Relative Humidity");
+
+						  svg.append("path")
+						      .datum(tsv_data)
+						      .attr("class", "line")
+						      .attr("d", line);
+			//end forEach()
+				});
+		
+			$scope.check_humidity_data = data;
+			}
+		//end drawgraph
+		}   
 
     //end scope
    }
